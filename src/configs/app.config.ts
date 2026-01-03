f763229d0c5bc24c7fad443c9d0e8df2c8574f86
Sandi Perdiansah <sandiperdiansah.info@gmail.com>
2026-01-03 18:56:11 +0700

    chore: update app configuration

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -3,12 +3,15 @@ import 'dotenv/config';
 
 export const PORT = Number(process.env.APP_PORT) || 3001;
 const CORS_ALLOWED_ORIGINS = process.env.CORS_ALLOWED_ORIGINS?.split(',') || [];
+const CORS_ALLOWED_METHODS = process.env.CORS_ALLOWED_METHODS?.split(',') || [];
+const CORS_ALLOWED_HEADERS = process.env.CORS_ALLOWED_HEADERS?.split(',') || [];
 
 export const appConfig: NestApplicationOptions = {
 	logger: ['verbose', 'log', 'warn', 'error'],
 	cors: {
 		origin: CORS_ALLOWED_ORIGINS,
 		credentials: true,
-		methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+		methods: CORS_ALLOWED_METHODS,
+		allowedHeaders: CORS_ALLOWED_HEADERS,
 	},
 };
